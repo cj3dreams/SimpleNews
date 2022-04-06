75dd6c5769eebc45be25474cf713c1619bafe2ff
3Dimensional <cj3dream@gmail.com>
2022-04-06 23:08:24 +0500

    Bottom Nav Bar
    -Icons
    -Designed Items
    -Added First and Main Home Fragment
    -Realized supportFragmentManager Code

--- app/src/main/java/com/twenty2byte/simplenews/MainActivity.kt
+++ app/src/main/java/com/twenty2byte/simplenews/MainActivity.kt
@@ -1,12 +1,14 @@
 
 package com.twenty2byte.simplenews
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarMenuView
 import com.google.android.material.navigation.NavigationBarView
+import com.twenty2byte.simplenews.view.HomeFragment
 
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var bottomNavigationView: BottomNavigationView
@@ -15,11 +17,24 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         bottomNavigationView = findViewById(R.id.bottom_nav_menu)
         bottomNavigationView.selectedItemId = R.id.mainHome
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        var fragment: Fragment? = null
+        when (item.itemId){
+            R.id.mainHome -> fragment = HomeFragment()
+        }
+        if (fragment != null) {
+            screenChanger(fragment)
+        }
+        return true
+    }
+
+    private fun screenChanger(fragment: Fragment){
+        supportFragmentManager.beginTransaction().
+        replace(R.id.frgView, fragment).commit()
     }
 }
